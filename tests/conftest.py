54df8fea914e910927c9e14cb8b20cdfa1e21dac
Oldrich Klimanek <oldrich.klimanek@gmail.com>
2025-03-03 23:28:51 +0100

    Ignore real_example.py
    Excluding real_example.py from pytest scope.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import shutil
 import pytest
+from pathlib import Path
 
 from crodl.settings import DOWNLOAD_PATH
 
@@ -11,3 +12,7 @@ def cleanup_test_dirs():
 
     shutil.rmtree(DOWNLOAD_PATH / "audio_title", ignore_errors=True)
     shutil.rmtree(DOWNLOAD_PATH / "Some Title", ignore_errors=True)
+
+
+def pytest_ignore_collect(collection_path: Path):
+    return collection_path.name == "real_example.py"
