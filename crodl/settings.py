361b143e30e935446642b58820b03803c137936e
Oldrich Klimanek <oldrich.klimanek@gmail.com>
2025-03-13 17:51:57 +0100

    Add SERIES_DOWNLOAD_DIR
    Fix the hardcoded dir name, move to `settings.py`.

--- crodl/settings.py
+++ crodl/settings.py
@@ -5,6 +5,7 @@ HOME_DIR = Path.home()
 DOWNLOAD_DIR = "Z Rozhlasu"
 SEGMENTS_SUBDIR = ".chunks"
 DOWNLOAD_PATH = HOME_DIR / DOWNLOAD_DIR
+SERIES_DOWNLOAD_DIR = "Seriály"
 LOG_PATH = DOWNLOAD_PATH / "logs"
 SUPPORTED_DOMAINS = ("www.mujrozhlas.cz", "mujrozhlas.cz")
 SUPPORTED_AUDIO_FORMATS = ("aac", "m4a")
